6e75539ead1dcd8c6d18d019bdc0c499e2bac9ef
guuhx97 <gustavorobertotb@outlook.com>
2019-10-07 12:02:02 -0300

    Frontend - feito ajustes na tela de logs

--- frontend/src/pages/Logs/Log.js
+++ frontend/src/pages/Logs/Log.js
@@ -9,81 +9,101 @@ export default function Logs() {
       <Menu />
       <div className="container-central">
         <div className="container-datas">
+            <div className="data-fixa">
+              <p>
+                <strong className="fixoOne">Data</strong>
+                <strong className="fixoTwo">IP Local</strong>
+                <strong className="fixoThree">IP Global</strong>
+                <strong className="fixoFour">Portas</strong>
+                <strong className="fixoFive">Status</strong>
+              </p> 
+            </div>
+
             <div className="data">
               <p>
-                <strong>Data: </strong> 03/10/2019
-                <strong>IP Local: </strong> 100.64.0.10
-                <strong>IP Global: </strong> 177.74.241.10
-                <strong>Portas: </strong>  5000-7999  
+                <strong className="infoOne">03/10/2019 - 00/00/0000</strong>
+                <strong className="infoTwo">255.255.255.255</strong>
+                <strong className="infoThree">255.255.255.255</strong>
+                <strong className="infoFour">50000-79990</strong>
+                <strong className="infoFiveAtivado">Ativado</strong>
               </p>             
             </div>
             <div className="data">
               <p>
-                <strong>Data: </strong> 03/10/2019
-                <strong>IP Local: </strong> 100.64.0.10
-                <strong>IP Global: </strong> 177.74.241.10
-                <strong>Portas: </strong>  5000-7999  
+                <strong className="infoOne">03/10/2019 - 00/00/0000</strong>
+                <strong className="infoTwo">255.255.255.255</strong>
+                <strong className="infoThree">255.255.255.255</strong>
+                <strong className="infoFour">50000-79990</strong>
+                <strong className="infoFiveAtivado">Ativado</strong>
               </p>             
             </div>
             <div className="data">
               <p>
-                <strong>Data: </strong> 03/10/2019
-                <strong>IP Local: </strong> 100.64.0.10
-                <strong>IP Global: </strong> 177.74.241.10
-                <strong>Portas: </strong>  5000-7999  
+                <strong className="infoOne">03/10/2019 - 00/00/0000</strong>
+                <strong className="infoTwo">255.255.255.255</strong>
+                <strong className="infoThree">255.255.255.255</strong>
+                <strong className="infoFour">50000-79990</strong>
+                <strong className="infoFiveAtivado">Ativado</strong>
               </p>             
             </div>
             <div className="data">
               <p>
-                <strong>Data: </strong> 03/10/2019
-                <strong>IP Local: </strong> 100.64.0.10
-                <strong>IP Global: </strong> 177.74.241.10
-                <strong>Portas: </strong>  5000-7999  
+                <strong className="infoOne">03/10/2019 - 07/10/2019</strong>
+                <strong className="infoTwo">255.255.255.255</strong>
+                <strong className="infoThree">255.255.255.255</strong>
+                <strong className="infoFour">50000-79990</strong>
+                <strong className="infoFiveDesativado">Desativado</strong>
               </p>             
             </div>
             <div className="data">
               <p>
-                <strong>Data: </strong> 03/10/2019
-                <strong>IP Local: </strong> 100.64.0.10
-                <strong>IP Global: </strong> 177.74.241.10
-                <strong>Portas: </strong>  5000-7999  
+                <strong className="infoOne">03/10/2019 - 05/10/2019</strong>
+                <strong className="infoTwo">255.255.255.255</strong>
+                <strong className="infoThree">255.255.255.255</strong>
+                <strong className="infoFour">50000-79990</strong>
+                <strong className="infoFiveDesativado">Desativado</strong>
               </p>             
             </div>
             <div className="data">
               <p>
-                <strong>Data: </strong> 03/10/2019
-                <strong>IP Local: </strong> 100.64.0.10
-                <strong>IP Global: </strong> 177.74.241.10
-                <strong>Portas: </strong>  5000-7999  
+                <strong className="infoOne">03/10/2019 - 00/00/0000</strong>
+                <strong className="infoTwo">255.255.255.255</strong>
+                <strong className="infoThree">255.255.255.255</strong>
+                <strong className="infoFour">50000-79990</strong>
+                <strong className="infoFiveAtivado">Ativado</strong>
               </p>             
             </div>
             <div className="data">
               <p>
-                <strong>Data: </strong> 03/10/2019
-                <strong>IP Local: </strong> 100.64.0.10
-                <strong>IP Global: </strong> 177.74.241.10
-                <strong>Portas: </strong>  5000-7999  
+                <strong className="infoOne">03/10/2019 - 06/10/2019</strong>
+                <strong className="infoTwo">255.255.255.255</strong>
+                <strong className="infoThree">255.255.255.255</strong>
+                <strong className="infoFour">50000-79990</strong>
+                <strong className="infoFiveDesativado">Desativado</strong>
               </p>             
             </div>
             <div className="data">
               <p>
-                <strong>Data: </strong> 03/10/2019
-                <strong>IP Local: </strong> 100.64.0.10
-                <strong>IP Global: </strong> 177.74.241.10
-                <strong>Portas: </strong>  5000-7999  
+                <strong className="infoOne">03/10/2019 - 00/00/0000</strong>
+                <strong className="infoTwo">255.255.255.255</strong>
+                <strong className="infoThree">255.255.255.255</strong>
+                <strong className="infoFour">50000-79990</strong>
+                <strong className="infoFiveAtivado">Ativado</strong>
               </p>             
             </div>
-            <div className="data">
-              <p>
-                <strong>Data: </strong> 03/10/2019
-                <strong>IP Local: </strong> 100.64.0.10
-                <strong>IP Global: </strong> 177.74.241.10
-                <strong>Portas: </strong>  5000-7999  
-              </p>             
-            </div>
+            
         </div>
         <div className="container-filter">
-          <h1>Hello World</h1>
+          <h1>Filtrar</h1>
+          <p>Endereço IP: <br /><input type="text" /></p>
+          <p>Porta: <br /><input type="text" /></p>
+          <p>Status:
+            <select >
+              <option selected>Ativo</option>
+              <option>Desativado</option>
+            </select>
+          </p>
+          <button>Filtrar</button>
         </div>
       </div>
     </div>
