394c51b6ce1c2b439e8c4db93837746cb29efa0e
guuhx97 <gustavorobertotb@outlook.com>
2019-10-10 20:44:40 -0300

    Back/Front - finalizado busca na tela home do sistema

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,61 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Menu/Menu';
 
+import api from '../../services/api';
+
 import './Home.css';
-  
+
 export default function Pricing() {
+  const [interfacesList, setInterfacesList] = useState([]);
+
+  useEffect(() => {
+    async function loadHome(){
+        const response = await api.get('/home', {})
+        setInterfacesList(response.data)
+        console.log(response.data)
+    }
+    loadHome();
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
+
   return (
       <div className="container">
         <Menu />
         <div className="container-central">  
           <div className="containerLeft">
             <h1>Atividades Recentes</h1>
-            <p className="mensagem"><strong>Adicionado</strong><br/>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfegou uma bandeja de tipos.
+            <div className="mensagem"><strong>Adicionado</strong><br/>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfegou uma bandeja de tipos.
               <p className="dataContainer">Thiago - 02-10-2019 13:30</p>
-            </p>
-            <p className="mensagem"><strong>Modificado</strong><br/>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfegou uma bandeja de tipos.
+            </div>
+            <div className="mensagem"><strong>Modificado</strong><br/>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfegou uma bandeja de tipos.
               <p className="dataContainer">João - 02-10-2019 13:30</p>
-            </p>
-            <p className="mensagem"><strong>Modificado</strong><br/>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfegou uma bandeja de tipos.
+            </div>
+            <div className="mensagem"><strong>Modificado</strong><br/>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfegou uma bandeja de tipos.
               <p className="dataContainer">João - 02-10-2019 13:30</p>
-            </p>
-            <p className="mensagem"><strong>Modificado</strong><br/>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfegou uma bandeja de tipos.
+            </div>
+            <div className="mensagem"><strong>Modificado</strong><br/>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfegou uma bandeja de tipos.
               <p className="dataContainer">João - 02-10-2019 13:30</p>
-            </p>
+            </div>
             
           </div>    
           <div className="containerMiddle">
             <h1>Informações CGNAT</h1>
-            <p className="mensagem"><strong>Placa de Rede: <strong>enp0s1 </strong></strong><br/>
-              <p className="information"><strong>Endereço IPv4:</strong>100.64.10.1/24</p>
-              <p className="information"><strong>Mascara de Rede</strong>255.255.255.255</p>
-              <p className="information"><strong>Pacotes Enviados:</strong>3000 pacotes</p>
-              <p className="information"><strong>Pacotes Recebidos:</strong>800 pacotes</p> 
-              <p className="information"><strong>Erros Recebimento:</strong>800 pacotes</p> 
-              <p className="information"><strong>Erros Transmissão:</strong>800 pacotes</p>  
-            </p>
-            <p className="mensagem"><strong>Placa de Rede: <strong>enp0s2 </strong></strong><br/>
-            <p className="information"><strong>Endereço IPv4:</strong>100.64.10.1/24</p>
-              <p className="information"><strong>Mascara de Rede</strong>255.255.255.255</p>
-              <p className="information"><strong>Pacotes Enviados:</strong>3000 pacotes</p>
-              <p className="information"><strong>Pacotes Recebidos:</strong>800 pacotes</p> 
-              <p className="information"><strong>Erros Recebimento:</strong>800 pacotes</p> 
-              <p className="information"><strong>Erros Transmissão:</strong>800 pacotes</p>  
-            </p>
+
+          {
+            interfacesList.map((element, index) => {       
+              // eslint-disable-next-line no-unused-expressions
+              return (<div key={index} className="mensagem"><strong>Placa de Rede:{element.name}</strong><br/>
+                <p className="information"><strong>Endereço IPv4:</strong>{element.address}</p>
+                <p className="information"><strong>Mascara de Rede</strong>{element.mask}</p>
+                <p className="information"><strong>Pacotes Enviados:</strong>{`${element.packageTx} Pacotes`}</p>
+                <p className="information"><strong>Pacotes Recebidos:</strong>{`${element.packageRx} Pacotes`}</p> 
+                <p className="information"><strong>Erros Recebimento:</strong>{`${element.errorsRx} Pacotes`}</p> 
+                <p className="information"><strong>Erros Transmissão:</strong>{`${element.errorsTx} Pacotes`}</p>  
+              </div>)
+            })
+          }
           </div>  
           <div className="containerRight">
             <h1>Prefixos</h1>
-            <p className="mensagem">
+            <div className="mensagem">
               <strong>Públicos</strong>
               <p>177.74.240.0/20</p>
               <p>177.74.240.0/20</p>
               <p>177.74.240.0/20</p>
               <p>177.74.240.0/20</p>
-            </p>
-            <p className="mensagem">
+            </div>
+            <div className="mensagem">
               <strong>Privado</strong>
               <p>100.64.0.0/16</p>
-            </p>
+            </div>
           </div>
        </div>
       </div>
