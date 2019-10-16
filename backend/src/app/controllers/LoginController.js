8dccf95286d57df3aaf132f750d5c90defbc81fb
guuhx97 <gustavorobertotb@outlook.com>
2019-10-16 16:26:04 -0300

    Backend - feito rota que retorna auth

---
+++ backend/src/app/controllers/LoginController.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import sha1 from 'sha1';
+
+class LoginController {
+    async index(req,res) {
+        const { user, password } = req.body;
+       
+       const password_hash = sha1(password);
+        let listUserSeparada = [];
+        fs.readFile('archive/auth', function(err, data) {
+            if(err){
+                console.log(err)
+            }else{
+                const listUser = data.toString('utf8').split('\n');
+                listUserSeparada =  listUser.map(e => e = e.split('<->'))
+                for(let i = 0; i < listUserSeparada.length; i++){
+                    console.log(`Senha Cadastrada ${listUserSeparada[i][1]}`)
+                    if(listUserSeparada[i][0] === user && listUserSeparada[i][1] === password_hash){
+                        return res.json({"auth": true});
+                    }
+                }
+                return res.status(400).json({"auth": false})
+            }
+        })
+    }
+}
+
+export default new LoginController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import HomeController from './app/controllers/HomeController'
-
+import LoginController from './app/controllers/LoginController';
 const routes = new Router();
 
 routes.get('/home', HomeController.index)
+routes.post('/login', LoginController.index)
 
 export default routes;
