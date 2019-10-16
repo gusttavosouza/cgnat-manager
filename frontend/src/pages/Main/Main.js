5d622339391aa0eb8d8bea64aa895c677adca930
guuhx97 <gustavorobertotb@outlook.com>
2019-10-15 23:17:27 -0300

    Frontend - feito roteamento entre o main e o login

--- frontend/src/pages/Main/Main.js
+++ frontend/src/pages/Main/Main.js
@@ -1,41 +1,8 @@
-import React, { useState } from 'react';
-import fetch from 'node-fetch';
-
-import './Main.css';    
+import React from 'react';
+import { Redirect } from 'react-router-dom';  
 
 export default function Main() {
-
-    const [command, setCommand] = useState('');
-
-    async function handleSubmit() {
-        const response = await fetch('http://localhost:3030/', {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({
-                "command": command
-            })
-        });
-        const resposta = await response.json()
-        console.log(resposta)
-    }
-
     return (
-        <div>
-            <div className="execute-container">
-                <input
-                    placeholder="Informa o comando Iptables"
-                    value={command}
-                    onChange={e => setCommand(e.target.value)}
-                />
-                <button type="submit" onClick={handleSubmit}>EXECUTAR</button>
-            </div>
-
-            <div className="title-container">
-                <div>Logs</div>
-            </div>
-            <div className="response-container">          
-                <div />
-            </div>
-        </div>
+        <Redirect to='/login' />
     )
 }
