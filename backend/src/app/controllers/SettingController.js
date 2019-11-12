95767219dca5c123f96b6272b594b66b7456d37b
guuhx97 <gustavorobertotb@outlook.com>
2019-11-11 22:57:22 -0300

    Ajustado problema relaciona a busca de IP curtos

--- backend/src/app/controllers/SettingController.js
+++ backend/src/app/controllers/SettingController.js
@@ -4,47 +4,51 @@ class SettingController {
   async index(req,res){
     const comandos = shelljs.exec("sudo iptables -t nat -L -n | grep tcp");
     const vetRoles = comandos.split('\n');
-    const vetRolesSeparados = vetRoles.map(e => e = e.split(" "));
-
-    let listRoles = []
-    for(let i = 0; i < vetRolesSeparados.length - 1; i++){
-      let resto = vetRolesSeparados[i][33].split(':');
-      listRoles.push([
-       vetRolesSeparados[i][11],
-       resto[1], resto[2],"SIM"
-      ])
-    }
-    return res.json(listRoles);
+    let publicoMaisPorta = "";
+    let privado = ""; 
+    let lista = [];
+    for(let i = 0; i < vetRoles.length-1; i++){
+      for(let j = 20; j < vetRoles[i].length; j++){
+          if(vetRoles[i][j] !== " "){
+            privado += vetRoles[i][j]
+          }else{
+            break
+          }
+      }
+      for(let j = 65; j < vetRoles[i].length; j++){
+        if(vetRoles[i][j] !== " "){
+          publicoMaisPorta += vetRoles[i][j]
+        }else{
+          break
+        }
+      }
+      const vetNew = publicoMaisPorta.split(":");
+      lista.push([privado, vetNew[0], vetNew[1], "SIM"])
+      publicoMaisPorta = "";
+      privado = ""
+    };
+    return res.json(lista);
   }
 
   async store(req,res){
     const { listConfig, listDelete }  = req.body;
-
     if(listConfig){
       listConfig.forEach(e => {
         if(e[3] === "NÃO"){
           shelljs.exec(`sudo iptables -t nat -A POSTROUTING -s ${e[0]} -p tcp -j SNAT --to ${e[1]}:${e[2]}`)
-          console.log(`sudo iptables -t nat -A POSTROUTING -s ${e[0]} -p tcp -j SNAT --to ${e[1]}:${e[2]}`)
           shelljs.exec(`sudo iptables -t nat -A POSTROUTING -s ${e[0]} -p udp -j SNAT --to ${e[1]}:${e[2]}`)
         }
       });
     }
-
     if(listDelete.length !== 0){
-      console.log('entrou para excluir', listDelete)
       listDelete.forEach(e => {
         if(e[3] === "SIM"){
           shelljs.exec(`sudo iptables -t nat -D POSTROUTING -s ${e[0]} -p tcp -j SNAT --to ${e[1]}:${e[2]}`)
-          console.log(`sudo iptables -t nat -D POSTROUTING  -s ${e[0]} -p tcp -j SNAT --to ${e[1]}:${e[2]}`)
           shelljs.exec(`sudo iptables -t nat -D POSTROUTING -s ${e[0]} -p udp -j SNAT --to ${e[1]}:${e[2]}`)
         }
       });
     }
     return res.json({ok: true});
-  }
-
-  async delete(req, res){
-
   }
 }
 
